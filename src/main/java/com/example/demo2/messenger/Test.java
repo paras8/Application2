061472fb5443b740dc9476f42f8b5355bf4c2217
solanki-paras19 <solankiparas621@gmail.com>
2025-04-15 21:59:21 +0530

    added new files for testing

---
+++ src/main/java/com/example/demo2/messenger/Test.java
@@ -0,0 +1,6 @@
+package com.example.demo2.messenger;
+
+public class Test {
+
+    String myname;
+}
--- src/main/java/com/example/demo2/messenger/TestingMerge.java
+++ src/main/java/com/example/demo2/messenger/TestingMerge.java
@@ -2,6 +2,10 @@ package com.example.demo2.messenger;
 
 public class TestingMerge {
 
+
+    public void test(){
+        System.out.println("");
+    }
     public void print(){
         System.out.println("Test");
     }
